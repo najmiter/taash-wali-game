1d972a0d81dbc09b0d00f028009f4fc4b764aa54
Najam <realnajmiter@gmail.com>
2026-05-03 21:06:56 +0500

    fix new top card on completed deck animation

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1777822954|1979995787';
+const CACHE_VERSION = '1777824390|1083904584';
 /** @type {string} */
 const CACHE_PREFIX = 'Solitaire-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
